6302c84c05b7ca3b3313d6aad4bdbd785d4c55f5
Alexey Hrynevich <a.hrynevich@sumatosoft.com>
2019-07-22 14:15:00 +0300

    Implement chat view(FE part)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { API_WS_ROOT } from './constants';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ActionCableProvider url={API_WS_ROOT}>
+    <App />
+  </ActionCableProvider>,
+  document.getElementById('root')
+);
