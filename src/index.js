b6d20eb64b0eba55833fe75b2d41166bd179dfd0
Alexey Hrynevich <a.hrynevich@sumatosoft.com>
2019-07-25 14:53:44 +0300

    Add some fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ActionCableProvider } from 'react-actioncable-provider';
+import ActionCable from 'actioncable';
 import './styles/index.css';
 import App from './containers/App';
 import { API_WS_ROOT } from './constants/constants';
 
+const cable = ActionCable.createConsumer(API_WS_ROOT)
 
 ReactDOM.render(
-  <ActionCableProvider url={API_WS_ROOT}>
+  <ActionCableProvider cable={cable}>
     <App />
   </ActionCableProvider>,
   document.getElementById('root')
